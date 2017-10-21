7e384c2d30f5c441bb589dae33498ddbf0a169dc
HH8023 <wp_0357@163.com>
2017-10-21 18:04:52 +0000

    user

--- blog/app/Http/routes.php
+++ blog/app/Http/routes.php
@@ -15,6 +15,32 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// 后台路由群组
+Route::group(['prefix'=>'admin','namespace'=>'Admin'], function (){
+
+    // Route::get('/', function ()    {
+    //     // 使用 Auth 中间件
+    // });
+// 'middleware'=>'login'
+	// // 用户模块
+ //    Route::get('user/profile', 'admin');
 
 
+
+	// 后台首页路由
+	Route::get('index','IndexController@index');
+	// Route::get('info','IndexController@info');
+
+    // 用户模块
+    Route::get('user', 'UserController@index');
+    Route::get('user/{id}', 'UserController@auth');
+	Route::post('user/add', 'UserController@add');
+
+
+
+
+
+
+
+});
 
